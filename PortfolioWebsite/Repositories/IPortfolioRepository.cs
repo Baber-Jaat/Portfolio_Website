ac9dc3be1a0640e1a52242aa594e0a3a5e0a9764
Baber-Jaat <jaatbaber@gmail.com>
2024-11-19 11:31:57 +0500

    Initial commit with .NET Core project

---
+++ PortfolioWebsite/Repositories/IPortfolioRepository.cs
@@ -0,0 +1,14 @@
+﻿using PortfolioWebsite.Models;
+
+
+namespace PortfolioWebsite.Repositories
+{
+    public interface IPortfolioRepository
+    {
+        Task<IEnumerable<PortfolioItem>> GetAllItemsAsync(); 
+        Task AddItemAsync(PortfolioItem item);
+        Task<PortfolioItem> GetItemByIdAsync(int id);
+        Task UpdateItemAsync(PortfolioItem item);
+        Task DeleteItemAsync(int id);
+    }
+}
